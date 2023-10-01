439f5f5da1919443188fa688c047b1194435dc6f
jo-project <jo.project.0911@gmail.com>
2023-10-02 08:43:42 +1100

    FM

--- lua/fm/init.lua
+++ lua/fm/init.lua
@@ -326,7 +326,7 @@ M.open_float = function(dir)
       if config.adapters[scheme] == "files" then
         assert(path)
         local fs = require("fm.fs")
-        title = vim.fn.fnamemodify(fs.posix_to_os_path(path), ":~")
+        title = "FM - "..vim.fn.fnamemodify(fs.posix_to_os_path(path), ":~") 
       end
       return title
     end
